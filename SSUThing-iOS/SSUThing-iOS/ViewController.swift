4f3b87a368d773f7f20c6f1e4db162e2b65920ff
최지우 <cjw@choejiuui-MacBookPro.local>
2023-02-01 18:22:56 +0900

    [Setting] Project Setting

---
+++ SSUThing-iOS/SSUThing-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SSUThing-iOS
+//
+//  Created by 최지우 on 2023/02/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
