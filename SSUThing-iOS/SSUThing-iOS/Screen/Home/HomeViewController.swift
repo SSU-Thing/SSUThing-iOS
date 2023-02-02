80130fab977f4eaf00285adfb0316e6fe6ea9183
최지우 <cjw@choejiuui-MacBookPro.local>
2023-02-03 04:16:35 +0900

    [Feat] Add Select Start/End Date #5

--- SSUThing-iOS/SSUThing-iOS/Screen/Home/HomeViewController.swift
+++ SSUThing-iOS/SSUThing-iOS/Screen/Home/HomeViewController.swift
@@ -10,7 +10,7 @@ import SnapKit
 
 class HomeViewController : UIViewController {
     
-    //UIView
+    // WelcomeLabel
     private let UserWelcomeLabel : UILabel = {
         let label = UILabel()
         label.text = "김동현님\nSSUThing에 오신걸 환영해요!"
@@ -21,7 +21,44 @@ class HomeViewController : UIViewController {
         return label
     }()
 
+    // Select Date
+    private let RentalStartDateLabel : UILabel = {
+        let label = UILabel()
+        label.text = "빌릴 날짜 : "
+        return label
+    }()
+
+    private let RentalEndDateLabel : UILabel = {
+        let label = UILabel()
+        label.text = "반납 날짜 : "
+        return label
+    }()
+
+    private let startDatePicker = UIDatePicker()
+    private let endDatePicker = UIDatePicker()
     
+
+    // Select Date _ StackView
+    lazy var RentalStartStackView : UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [RentalStartDateLabel,startDatePicker])
+        stackView.axis = .horizontal
+        return stackView
+    }()
+
+    lazy var RentalEndStackView : UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [RentalEndDateLabel,endDatePicker])
+        stackView.axis = .horizontal
+        return stackView
+    }()
+
+    lazy var SelectDateStackView : UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [RentalStartStackView,RentalEndStackView])
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        return stackView
+    }()
+
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,11 +71,14 @@ class HomeViewController : UIViewController {
     private func configureUI() {
         setViewHierarchy()
         setConstrains()
+        setCalenderStyle(startDatePicker)
+        setCalenderStyle(endDatePicker)
     }
     
     
     func setViewHierarchy(){
         view.addSubview(UserWelcomeLabel)
+        view.addSubview(SelectDateStackView)
     }
     
     func setConstrains(){
@@ -46,10 +86,45 @@ class HomeViewController : UIViewController {
         UserWelcomeLabel.snp.makeConstraints { make in
             make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).offset(20)
             make.leading.equalToSuperview().offset(25)
-            
+        }
+        SelectDateStackView.snp.makeConstraints { make in
+            make.top.equalTo(UserWelcomeLabel.snp.bottom).offset(30)
+            make.leading.equalToSuperview().offset(25)
+
         }
     }
+    
+    private func setCalenderStyle(_ targetDate: UIDatePicker) {
+        targetDate.preferredDatePickerStyle = .compact
+        targetDate.datePickerMode = .dateAndTime
+        targetDate.locale = Locale(identifier: "ko-KR")
+        targetDate.timeZone = .autoupdatingCurrent
+        
+        // 분 단위 설정이 30분 간격
+        targetDate.minuteInterval = 30
+        
+        // 임시 날짜 비활성화
+        var components = DateComponents()
+        components.day = 10
+        let maxDate = Calendar.autoupdatingCurrent.date(byAdding: components, to: Date())
+        components.day = -10
+        let minDate = Calendar.autoupdatingCurrent.date(byAdding: components, to: Date())
+
+        targetDate.maximumDate = maxDate
+        targetDate.minimumDate = minDate
+
+
+        targetDate.addTarget(self, action: #selector(handleDatePicker(_:)), for: .valueChanged)
+    }
+    
+    // MARK: - Selectors
+    @objc
+    private func handleDatePicker(_ sender: UIDatePicker) {
+        print(sender.date)
+    }
 }
+
+
 
 
 // MARK: - PreView
