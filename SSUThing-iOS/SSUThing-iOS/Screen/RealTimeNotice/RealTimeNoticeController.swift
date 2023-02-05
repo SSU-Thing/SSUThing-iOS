c3c585b1fb8901749ddf3f93a06497a19e1b92af
최지우 <cjw@choejiuui-MacBookPro.local>
2023-02-06 03:58:27 +0900

    [Add] Add RealTimeNotice File #5

---
+++ SSUThing-iOS/SSUThing-iOS/Screen/RealTimeNotice/RealTimeNoticeController.swift
@@ -0,0 +1,13 @@
+//
+//  RealTimeNoticeController.swift
+//  SSUThing-iOS
+//
+//  Created by 최지우 on 2023/02/06.
+//
+
+import UIKit
+import SnapKit
+
+class RealTimeNoticeController : UIViewController {
+    
+}
