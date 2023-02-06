ade287c15fc97c8d9501c1d8727305367568240e
최지우 <cjw@choejiuui-MacBookPro.local>
2023-02-06 18:29:53 +0900

    [Add] Add UIColor+

--- SSUThing-iOS/SSUThing-iOS/Global/Extension/UIColor+.swift
+++ SSUThing-iOS/SSUThing-iOS/Global/Extension/UIColor+.swift
@@ -11,6 +11,10 @@ import UIKit
 import SnapKit
 
 enum CustomColor {
+    case lightgreen
+    case whitegray
+    case lightgray
+    case darkGray
     case maingreen
     case mainbackgroundcolor
 }
@@ -19,6 +23,18 @@ extension UIColor {
     
     static func customColor(_ color: CustomColor) -> UIColor {
             switch color {
+            case .lightgreen:
+                return UIColor(red: 0.34, green: 0.83, blue: 0.86, alpha: 1.00)
+                
+            case .whitegray:
+                return UIColor(red: 0.94, green: 0.94, blue: 0.94, alpha: 1.00)
+                
+            case .lightgray:
+                return UIColor(red: 0.64, green: 0.64, blue: 0.64, alpha: 1.00)
+                
+            case .darkGray:
+                return UIColor(red: 0.65, green: 0.65, blue: 0.65, alpha: 1.00)
+                
             case .maingreen:
                 return UIColor(red: 0.41, green: 0.80, blue: 0.76, alpha: 1.00)
             
---
+++ SSUThing-iOS/SSUThing-iOS/Screen/Login/LoginViewController.swift
@@ -0,0 +1,8 @@
+//
+//  LoginViewController.swift
+//  SSUThing-iOS
+//
+//  Created by 최지우 on 2023/02/06.
+//
+
+import Foundation
