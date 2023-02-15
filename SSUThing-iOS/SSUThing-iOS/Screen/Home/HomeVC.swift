90efd1f2526b9ed54d12b7c1eb637c706cbf4a4d
최지우 <cjw@choejiuui-MacBookPro.local>
2023-02-15 22:34:43 +0900

    [Add] Add Assets

---
+++ SSUThing-iOS/SSUThing-iOS/Screen/Home/HomeVC.swift
@@ -0,0 +1,8 @@
+//
+//  HomeVC.swift
+//  SSUThing-iOS
+//
+//  Created by 최지우 on 2023/02/14.
+//
+
+import Foundation
---
+++ SSUThing-iOS/SSUThing-iOS/Screen/Home/MyRentalList.swift
@@ -0,0 +1,8 @@
+//
+//  MyRentalList.swift
+//  SSUThing-iOS
+//
+//  Created by 최지우 on 2023/02/15.
+//
+
+import Foundation
